5ffc4b427ea9b88527c1dae86b3775fecf897a71
Jan Issel <jan.issel@tuhh.de>
2021-01-29 22:45:45 +0100

    Added functionality to find start and end line of respective patent part

--- Utilities/statistics.py
+++ Utilities/statistics.py
@@ -6,7 +6,7 @@ import pathlib
 import pandas as pd
 from Utilities.directories import lexis_data
 
-feature_list = ["claim", "description", "abstract", "citation"]
+feature_list = ["claims", "description", "abstract", "references-cited"]
 
 
 def search_features(file):
@@ -17,15 +17,40 @@ def search_features(file):
     """
 
     with open(file, encoding='utf-8') as patent:
-
-        string = patent.read() # read file
+        all_lines = [line for line in patent]
+        n_lines = len(all_lines)
 
         occurrences = np.zeros([len(feature_list)], dtype=bool)  # 4 feature occurences are checked
+        start_lines = np.zeros([len(feature_list)], dtype=float)
+        end_lines = np.zeros([len(feature_list)], dtype=float)
+
         # check feature occurrence
         for i, feature in enumerate(feature_list):
-            if re.search(f'<{feature}', string):
-                occurrences[i] = 1
-    return occurrences
+            start_line = None
+            end_line = None
+            j = 0
+            while j < n_lines:
+                next_line = all_lines[j]
+                if re.search(f'(?=.*<{feature})(?=.*lang="eng").*', next_line):
+                    start_line = j
+                    j += 1
+                    next_line = all_lines[j]
+                    while not re.search(f'</{feature}>', next_line):
+                        j += 1
+                        next_line = all_lines[j]
+                    end_line = j
+                    break
+                j += 1
+            if start_line and end_line:
+                start_lines[i] = start_line
+                end_lines[i] = end_line
+                occurrences[i] = True
+            else:
+                start_lines[i] = np.nan
+                end_lines[i] = np.nan
+                occurrences[i] = False
+
+    return (occurrences, start_lines, end_lines)
 
 
 def feature_occurences(path_dataset):
@@ -39,9 +64,14 @@ def feature_occurences(path_dataset):
     path_list = pd.read_csv(path_dataset)['paths'].to_list()  # read all file paths
 
     occurrences = np.zeros([len(path_list), len(feature_list)], dtype=bool)
-    with ThreadPoolExecutor(max_workers=8) as executor:
-        for i, occ in enumerate(tqdm(executor.map(search_features, path_list), total=len(path_list))):
+    start_lines = np.zeros([len(path_list), len(feature_list)], dtype=float)
+    end_lines = np.zeros([len(path_list), len(feature_list)], dtype=float)
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        for i, package in enumerate(tqdm(executor.map(search_features, path_list[0:100]), total=len(path_list))):
+            occ, start, end = package
             occurrences[i, :] = occ
+            start_lines[i, :] = start
+            end_lines[i, :] = end
 
         occurence_sum = np.sum(occurrences, 0)
         relative_occurences = np.round(occurence_sum / len(path_list), 2)
@@ -51,6 +81,12 @@ def feature_occurences(path_dataset):
         stat_df = pd.DataFrame(index=patent_id)
         stat_df["path"] = path_list
         stat_df[feature_list] = occurrences
+        start_col_names = [f"{feature}-start" for feature in feature_list]
+        end_col_names = [f"{feature}-end" for feature in feature_list]
+        stat_df[start_col_names] = start_lines
+        stat_df[start_col_names] = stat_df[start_col_names].astype(pd.UInt32Dtype())
+        stat_df[end_col_names] = end_lines
+        stat_df[end_col_names] = stat_df[end_col_names].astype(pd.UInt32Dtype())
 
         return relative_occurences_dict, stat_df
 
@@ -83,6 +119,6 @@ if __name__ == '__main__':
     path_dataset_dir = pathlib.Path.joinpath(lexis_data, "paths.csv")
     ip7data = pathlib.Path.joinpath(lexis_data, "ip7-data-updated.csv")
     _, df1 = feature_occurences(path_dataset_dir)
-    _, df2 = label_occurences(ip7data, path_dataset_dir)
-    df3 = pd.concat([df1, df2], axis=1)
-    df3.to_csv("statistics.csv")
+    #_, df2 = label_occurences(ip7data, path_dataset_dir)
+    #df3 = pd.concat([df1, df2], axis=1)
+    df1.to_csv("statistics2.csv")
